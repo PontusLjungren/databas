3e79b6f50f44adfcecddf9cac608f0d5ee80f81e
PontusLjungren <pontus.ljungren@telia.com>
2021-10-05 18:17:21 +0200

    Print tables of our database

--- DBApp.py
+++ DBApp.py
@@ -3,7 +3,7 @@ import mysql.connector
 group_number="14" #FILL IN YOUR GROUP NUMBER
 
 mydb = mysql.connector.connect(
-  host="groucho.it.uu.se",
+  host="127.0.0.1",
   user="ht21_1_group_"+group_number,
   passwd="pwd_"+group_number,
   database="ht21_1_hotels_group_"+group_number
@@ -17,5 +17,17 @@ myresult = mycursor.fetchall()
 print("EANHotelID\t Name")
 for x in myresult:
   print(str(x[0])+"\t"+x[1])
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+  print(x)
+
+mycursor.execute("USE ht21_1_project_group_14")
+myresult = mycursor.fetchall()
+
+mycursor.execute("SHOW TABLES")
+for x in mycursor:
+  print(x)
 
 mydb.close()
