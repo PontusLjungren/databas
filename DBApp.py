07304a72510faa9e6b7afad23f618a52ebb25caf
johan <johandost@protonmail.com>
2021-10-11 15:25:10 +0200

    Extracted first program

--- DBApp.py
+++ DBApp.py
@@ -2,33 +2,18 @@ import mysql.connector
 
 import queries
 
-group_number="14" #FILL IN YOUR GROUP NUMBER
-mydb = mysql.connector.connect(
-  host="127.0.0.1",
-  user="ht21_1_group_"+group_number,
-  passwd="pwd_"+group_number,
-  database="ht21_1_hotels_group_"+group_number
-)
+def get_cursor():
+  group_number="14" #FILL IN YOUR GROUP NUMBER
+  mydb = mysql.connector.connect(
+    host="127.0.0.1",
+    user="ht21_1_group_"+group_number,
+    passwd="pwd_"+group_number,
+    database="ht21_1_hotels_group_"+group_number
+  )
 
-mycursor = mydb.cursor()
+  mycursor = mydb.cursor()
 
-mycursor.execute("USE ht21_1_project_group_14")
-mycursor.execute("SELECT department_number FROM DEPARTMENT")
-database_departments = []
-for x in mycursor:
-  database_departments.append(x[0])
 
-# get all department_number in a list.
-# check that input is valid
-while True:
-  department_id = input("What department do you wish to see?\n")
-  if int(department_id) in database_departments:
-    products = queries.is_leaf_department(mycursor, department_id)
-    for product in products:
-      output = ""
-      for attribute in product:
-        output = output + str(attribute) + " "
-      print(output)
 
 
 
---
+++ departments.py
@@ -0,0 +1,20 @@
+import DBApp
+import queries
+
+mycursor = DBApp.get_cursor()
+
+
+mycursor.execute("USE ht21_1_project_group_14")
+mycursor.execute("SELECT department_number FROM DEPARTMENT")
+database_departments = []
+for x in mycursor:
+  database_departments.append(x[0])
+while True:
+  department_id = input("What department do you wish to see?\n")
+  if int(department_id) in database_departments:
+    products = queries.is_leaf_department(mycursor, department_id)
+    for product in products:
+      output = ""
+      for attribute in product:
+        output = output + str(attribute) + " "
+      print(output)
